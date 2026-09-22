0469241867afd4f291657f59bf4021973bbcec94
Eliaszn <farellh12@gmail.com>
2026-09-22 19:01:07 +0700

    chore(cleanup): remove dead files and unused IRecurringTransactionEntity

--- lib/domain/repositories/recurring_repository.dart
+++ lib/domain/repositories/recurring_repository.dart
@@ -8,17 +8,3 @@ abstract class IRecurringRepository {
   Future<void> updateRecurring(RecurringTransaction recurring);
   Future<void> deleteRecurring(String id);
 }
-
-abstract class IRecurringTransactionEntity {
-  String get id;
-  String get transactionTemplateId;
-  String get frequency;
-  int get interval;
-  DateTime get startDate;
-  DateTime? get endDate;
-  DateTime get nextOccurrence;
-  bool get autoCreate;
-  bool get enabled;
-  DateTime get createdAt;
-  DateTime get updatedAt;
-}
