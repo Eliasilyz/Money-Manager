b498b6faf268626a6ccfe8d4fa1c96571e5088d8
Eliaszn <farellh12@gmail.com>
2026-09-23 15:03:23 +0700

    chore(config): update application bundle ID and display name
    - Update application ID / bundle identifier to `id.eliasilyz.moneymanager` across Android, iOS, macOS, and Linux
    - Change app display name from `money_manager_tmp` to `Money Manager` on all target platforms
    - Update publisher and copyright ownership to `eliasilyz`
    - Remove unused Kotlin `MainActivity.kt` files under temporary package paths

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.moneymanager.money_manager_tmp"
+    namespace = "id.eliasilyz.moneymanager"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -17,7 +17,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.moneymanager.money_manager_tmp"
+        applicationId = "id.eliasilyz.moneymanager"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
---
+++ android/app/src/main/kotlin/id/eliasilyz/moneymanager/MainActivity.kt
@@ -0,0 +1,5 @@
+package id.eliasilyz.moneymanager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
